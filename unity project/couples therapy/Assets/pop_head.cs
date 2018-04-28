7f49868aee9c6b4d39693d60f6fa308e3e609e6d
ryan fuller <ryan@codewizards.co.uk>
2018-04-28 22:37:26 +0100

    head now pops

---
+++ unity project/couples therapy/Assets/pop_head.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pop_head : MonoBehaviour {
+
+    public GameObject HeadToPop;
+    public Material[] MaterialsToReplace;
+
+	// Use this for initialization
+	void Start () {
+        PopHead(HeadToPop);
+	}
+	
+
+    private void PopHead(GameObject head){
+        var skinnedRenderer = head.GetComponent<SkinnedMeshRenderer>();
+        if(skinnedRenderer != null){
+            skinnedRenderer.enabled = false;
+        }
+        head.AddComponent<MeshRenderer>();
+        MeshRenderer meshRenderer = head.GetComponent<MeshRenderer>();
+        meshRenderer.materials = MaterialsToReplace;
+        head.AddComponent<Rigidbody>();
+        head.GetComponent<Rigidbody>().velocity = (new Vector3(1,2,0));
+    }
+}
